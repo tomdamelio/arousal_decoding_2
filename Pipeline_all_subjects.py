7f277ab9915e5490a5130c2ee08d9dc957daf646
tdamelio <tomas.d-amelio@inria.fr>
2021-03-24 03:07:19 -0300

    changes function experiment

--- Pipeline_all_subjects.py
+++ Pipeline_all_subjects.py
@@ -1,93 +1,139 @@
 #%%
 import os
 import os.path as op
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import mne
+import config as cfg
 
 from sklearn.dummy import DummyRegressor
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.linear_model import RidgeCV, GammaRegressor, BayesianRidge, TweedieRegressor, SGDRegressor
-from sklearn.svm import SVR
+#from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, GroupShuffleSplit, KFold, cross_val_predict, GridSearchCV, RandomizedSearchCV
 from sklearn.ensemble import RandomForestRegressor
+#from mne import Epochs
+#from mne.datasets.fieldtrip_cmc import data_path
+from joblib import Parallel, delayed
 
-import mne
-from mne import Epochs
-from mne.datasets.fieldtrip_cmc import data_path
-
-import config as cfg
 from library.spfiltering import (
     ProjIdentitySpace, ProjCommonSpace, ProjSPoCSpace)
 from library.featuring import Riemann, LogDiag, NaiveVec
 import library.preprocessing_david as pp
-
 from subject_number import subject_number
 
-from joblib import Parallel, delayed
+def run_low_rank(n_components, X, y, cv, estimators, scoring, groups):
+    out = dict(n_components=n_components)
+    for name, est in estimators.items(): #e.g. name --> riemann // est --> make_pipeline(...)
+        print(name)
+        this_est = est # est --> make_pipeline(...)
+        this_est.steps[0][1].n_compo = n_components #151 -> n_components inside Riemann inside pipeline
+        scores = cross_val_score(
+            X=X, y=y, cv=cv, estimator=this_est, n_jobs=1,
+            groups=groups,
+            scoring=scoring)
+        if scoring == 'neg_mean_absolute_error':
+            scores = -scores
+        print(np.mean(scores), f"+/-{np.std(scores)}")
+        out[name] = scores
+    return out
 
-class run_experiments:
+#%%
+def pipeline_all_subjects  (directory        =  'data',
+                            number_subject   =   subject_number,
+                            crop             =   True,
+                            extension        =  '.bdf',
+                            high_pass_filter =   0.01,
+                            shift_EDA        =   1.5,
+                            tune_components  =   True,
+                            target           =   'var',
+                            estimator        =   TweedieRegressor):
+
+    """
+
+    Reads the all subject files in 'data', preprocess signals, predicts EDA from EEG,
+    and outputs a .csv file per subject in 'outputs'
+
+    :directory:          String. Directory in which files are stored. Default = 'data'
+    :number_subject:     String or list. '01' to '32' (DEAP database).
+                         Default = 'subject_number' --> all subjects
+    :high_pass_filter:   Float. High-pass filter (in Hz). No filter = None.
+                         Default = 0.01               
+    :shift_EDA:          Float.    Length shift between EEG and EDA epochs (in seconds).
+                         Default = 1.5
+    :tune_components:    Boolean. Tune n_components (rank of cov matrices)
+                         Default = True                    
+    :target:             String.   'Y' used in our model 
+                         Default = 'var' --> var of EDA epochs
+    :estimator:          String.   Model used to predict EDA
+                         Default = TweedieRegressor
+                            
     
-    def __init__(self, directory, number_subject, high_pass_filter, shift_EDA,
-                 tune_components, target, model ):
-        self.directory           = directory
-        self.number_subject      = number_subject
-        self.high_pass_filter    = high_pass_filter
-        self.shift_EDA           = shift_EDA
-        self.tune_components     = tune_components
-        self.target              = target
-        self.model               = model
-        self.eda                 = eda
-        self.raw                 = raw
+    :return: .csv file per subject with predicted EDA (specifying other parameters)
+    
+    """ 
+    # container
+    exp = []
+    
+    # Return arguments and values passed to a function
+    func_args = locals()
+    
+    ###########################################################################
+    n_compo = 32
+    scale = 'auto'
+    metric = 'riemann'
+    seed = 42
+    n_splits = 2
+    n_jobs = 20
+    ###########################################################################
+
+    # Making the function work with only one subject as input
+    if type(number_subject) == str:
+        number_subject = [number_subject]
  
-    def preprocess (self, extension = 'bdf'):
-        """
-        Reads subject file in directory and preprocess signals
+    for i in number_subject:   
+    
+        fname = op.join(directory, 's'+ i + extension)
+        raw = mne.io.read_raw_bdf(fname, preload=True)
+        #(?) Is it necessary to preload or better to load_data? Why?
         
-        :return: preprocessed raw (EEG) and EDA of one subject 
-        """
+        # crop for memory purposes (use to test things)
+        if crop == True:
+            raw = raw.crop(100., 500.)
         
-        fname = op.join(self.directory, 's'+ self.number_subject + extension)
-        raw = mne.io.read_raw_bdf(fname, preload=True) #(?) Is it necessary to preload or load_data? Why?
+        picks_eda = mne.pick_channels(ch_names = raw.ch_names ,include=['GSR1'])
         
-        # crop for memory purposes (use to try things)
-        raw.crop(450., 650.)
+        if int(i) < 23:
+            raw.apply_function(fun=lambda x: x/1000, picks=picks_eda)
+        else:
+            raw.apply_function(fun=lambda x: (10**9/x)/1000, picks=picks_eda)
 
         # Separate EDA data
         eda = raw.copy().pick_channels(['GSR1'])
         
         # EDA Band-pass filter
-        eda.filter(self.high_pass_filter, 5, fir_design='firwin') 
+        eda.filter(high_pass_filter, 5, fir_design='firwin') 
 
         # Common channels
         common_chs = set(raw.info['ch_names'])
         
-        # Discard non-relevant channels #(?) Is it OK to descard EOG channels?
+        # Discard non-relevant channels 
         common_chs -= {'EXG1', 'EXG2', 'EXG3', 'EXG4',
                     'EXG5', 'EXG6', 'EXG7', 'EXG8',
                     'GSR2', 'Erg1', 'Erg2', 'Resp',
                     'Plet', 'Temp', 'GSR1', 'Status'}
+        #(?) Is it OK to descard EOG channels?
 
         # EEG Band-pass filter
         raw.pick_channels(list(common_chs))
         raw.filter(None, 120., fir_design='firwin')
         
-        return eda, raw
-
-    def compute_cov_matrices (self):
-        """
-        Inputs raw (EEG) and calculate covariance matrices ('X' in our model)
-        
-        :return: cov matrices of one subject's epochs
-        """
-        preprocess()
-        
         X = []
         
         for fb in pp.fbands:
-            rf = self.raw.copy().load_data().filter(fb[0], fb[1])
+            rf = raw.copy().load_data().filter(fb[0], fb[1])
             events = mne.make_fixed_length_events(rf,
                                                 id=3000,
                                                 duration=pp.duration,
@@ -97,6 +143,7 @@ class run_experiments:
                             event_id=3000, tmin=0, tmax=pp.duration,
                             proj=True, baseline=None, reject=None, preload=True, decim=1,
                             picks=None)
+            
             X.append([mne.compute_covariance(
                                             ec[ii], method='oas')['data'][None]
                                             for ii in range(len(ec))])   
@@ -108,130 +155,156 @@ class run_experiments:
         X = X.transpose(1,0,2,3)
                     
         n_sub, n_fb, n_ch, _ = X.shape
-            
-        return X, n_sub, n_fb, n_ch
-
-
-    def compute_model_output (self):
-        """
-        Inputs EDA and calculate model output ('y' in our model)
         
-        :return: 'y' time serie of one subject
-        """
-        events_shift = mne.make_fixed_length_events(self.eda, id=3000, start=1.5,
+        events_shift = mne.make_fixed_length_events(eda, id=3000, start=1.5,
                                                     duration=pp.duration, overlap=2.0)
 
-        eda_epochs_shift = Epochs(self.eda, events_shift, event_id=3000, tmin=0, tmax=pp.duration,
+        eda_epochs_shift = mne.Epochs(eda, events_shift, event_id=3000, tmin=0, tmax=pp.duration,
                                 proj=True, baseline=None, preload=True, decim=1)
         
-        if model == 'mean':
+        if target == 'mean':
             y = eda_epochs_shift.get_data().mean(axis=2)[:, 0]  
-        elif model == 'delta':
+        elif target == 'delta':
             y = eda_epochs_shift.get_data().max(axis=2)[:, 0] - eda_epochs_shift.get_data().min(axis=2)[:, 0]
         else:
             y = eda_epochs_shift.get_data().var(axis=2)[:, 0]     
             
-        return y
+        n_components = np.arange(1, 32, 1) # max components --> 32 --> 32 EEG channels
+        # now let's do group shuffle split
+        splits = np.array_split(np.arange(len(y)), n_splits)
+        groups = np.zeros(len(y), dtype=np.int)
+        for val, inds in enumerate(splits):
+            groups[inds] = val
 
+        ##################################################################
+        ridge_shrinkage = np.logspace(-3, 5, 100)
+        spoc_shrinkage = np.linspace(0, 1, 5)
+        common_shrinkage = np.logspace(-7, -3, 5)
+        ##################################################################
 
-    def run_low_rank(self, n_components, X, y, cv, estimators, scoring, groups):
-        out = dict(n_components=n_components)
-        for name, est in estimators.items():
-            print(name)
-            this_est = est
-            this_est.steps[0][1].n_compo = n_components
-            scores = cross_val_score(
-                X=X, y=y, cv=cv, estimator=this_est, n_jobs=1,
-                groups=groups,
-                scoring=scoring)
-            if scoring == 'neg_mean_absolute_error':
-                scores = -scores
-            print(np.mean(scores), f"+/-{np.std(scores)}")
-            out[name] = scores
-        return out
-
-
-    def low_rank(self):
-
-        low_rank_estimators = {k: v for k, v in pipelines.items()
-                            if k in ('riemann')} #'spoc', 
-
-        out_list = Parallel(n_jobs=n_jobs)(delayed(run_low_rank)(
-                            n_components=cc, X=X, y=y,
-                            groups=groups,
-                            cv=GroupShuffleSplit(
-                                n_splits=10, train_size=.8, test_size=.2),
-                            estimators=low_rank_estimators, scoring='r2')
-                            for cc in n_components)
-        out_frames = list()
-        for this_dict in out_list:
-            this_df = pd.DataFrame({#'spoc': this_dict['spoc'],
-                                'riemann': this_dict['riemann']})
-            this_df['n_components'] = this_dict['n_components']
-            this_df['fold_idx'] = np.arange(len(this_df))
-            out_frames.append(this_df)
-        out_df = pd.concat(out_frames)
-
-        out_df.to_csv("./DEAP_component_scores.csv")
-
-        mean_df = out_df.groupby('n_components').mean().reset_index()
+        pipelines = {
+            'dummy':  make_pipeline(
+                ProjIdentitySpace(), LogDiag(), StandardScaler(), DummyRegressor()),
+            'naive': make_pipeline(ProjIdentitySpace(), NaiveVec(method='upper'),
+                                StandardScaler(),
+                                RidgeCV(alphas=ridge_shrinkage)),
+            'log-diag': make_pipeline(ProjIdentitySpace(), LogDiag(),
+                                    StandardScaler(),
+                                    RidgeCV(alphas=ridge_shrinkage)),
+            'spoc': make_pipeline(
+                    ProjSPoCSpace(n_compo=n_compo,
+                                scale=scale, reg=0, shrink=0.5),
+                    LogDiag(),
+                    StandardScaler(),
+                    RidgeCV(alphas=ridge_shrinkage)),
+            'riemann':
+                make_pipeline(
+                    ProjCommonSpace(scale=scale, n_compo=n_compo, reg=1.e-05),
+                    Riemann(n_fb=n_fb, metric=metric),
+                    StandardScaler(),
+                    RidgeCV(alphas=ridge_shrinkage)),
+            'riemann_gamma': #GammaRegressor
+                make_pipeline(
+                    ProjCommonSpace(scale=scale, n_compo=n_compo, reg=1.e-05),
+                    Riemann(n_fb=n_fb, metric=metric),
+                    StandardScaler(),
+                    GammaRegressor())
+            }    
         
+        if tune_components == True:
+            
+            low_rank_estimators = {k: v for k, v in pipelines.items()
+                                if k in ('riemann')} #'spoc', 
 
-    def select_best_components(self):
-        best_components = {
-        #'spoc': mean_df['n_components'][mean_df['spoc'].argmax()],
-        'riemann': mean_df['n_components'][mean_df['riemann'].argmax()]
-        }
-        return best_components
+            out_list = Parallel(n_jobs=n_jobs)(delayed(run_low_rank)(
+                                n_components=cc, X=X, y=y,
+                                groups=groups,
+                                cv=GroupShuffleSplit(
+                                    n_splits=2, train_size=.5, test_size=.5),
+                                estimators=low_rank_estimators, scoring='r2')
+                                for cc in n_components)
+            out_frames = list()
+            for this_dict in out_list:
+                this_df = pd.DataFrame({#'spoc': this_dict['spoc'],
+                                    'riemann': this_dict['riemann']})
+                this_df['n_components'] = this_dict['n_components']
+                this_df['fold_idx'] = np.arange(len(this_df))
+                out_frames.append(this_df)
+            out_df = pd.concat(out_frames)
 
-    def models(self):
-        riemann_model = make_pipeline(
-        ProjCommonSpace(scale=scale, n_compo= 10, #best_components['riemann'],
-                        reg=1.e-05),
-        Riemann(n_fb=n_fb, metric=metric),
-        StandardScaler(),
-    #   RidgeCV(alphas=ridge_shrinkage))
-    #   GammaRegressor())
-    #   BayesianRidge())
-    #   TweedieRegressor())
-    #   SVR(kernel='poly', C=100, gamma='auto', degree=3, epsilon=.1,
-    #              coef0=1))
-    #  SGDRegressor())
-        estimator())
+            out_df.to_csv("./DEAP_component_scores.csv")
 
+            mean_df = out_df.groupby('n_components').mean().reset_index()
 
-    def pipeline_all_subjects  (self,
-                                directory        =  'data',
-                                number_subject   =  'all',
-                                high_pass_filter =   0.01, 
-                                shift_EDA        =   1.5,
-                                tune_components  =   True,
-                                target           =   'var',
-                                model            =  'TweedieRegressor'):
+            best_components = {
+            #'spoc': mean_df['n_components'][mean_df['spoc'].argmax()],
+            'riemann': mean_df['n_components'][mean_df['riemann'].argmax()]
+            }
 
-        """
-
-        Reads the all subject files in 'data', preprocess signals, predicts EDA from EEG,
-        and outputs a .csv file per subject in 'outputs'
-
-        :directory:          String.   directory in which files are stored. Default = data
-        :number_subject:     String.   '01' to '32' (DEAP database). Default = 'all'
-        :high_pass_filter:   Boolean.  True (0.01 Hz High-pass filter) or False (No filter --> None)
-                            Default = True
-        :shift_EDA:          Float.    Length shift between EEG and EDA epochs (in seconds).
-                            Default = 1.5
-        :target:             String.   Y used in our model 
-                            Default = 'mean' --> mean of EDA
-        :estimator:          String.   Model used to predict EDA
-                            Default = 'TweedieRegressor'
-                                
+            riemann_model = make_pipeline(
+            ProjCommonSpace(scale=scale, n_compo=best_components['riemann'],
+                            reg=1.e-05),
+            Riemann(n_fb=n_fb, metric=metric),
+            StandardScaler(),
+            estimator())
         
-        :return: .csv file per subject with predicted EDA (specifying other parameters)
-        """ 
+        else:
+            riemann_model = make_pipeline(
+            ProjCommonSpace(scale=scale, n_compo= 32,
+                            reg=1.e-05),
+            Riemann(n_fb=n_fb, metric=metric),
+            StandardScaler(),
+            estimator)
         
-        compute_cov_matrices ()
+        cv = KFold(n_splits=2, shuffle=False)
 
+        y_preds = cross_val_predict(riemann_model, X, y, cv=cv)
+        
+        r2_riemann_model = cross_val_score(riemann_model, X, y, cv=cv,  groups=groups)
+        print("mean of R2 cross validation Riemannian Model : ", np.mean(r2_riemann_model))
+        
+        for scoring in ("r2", "neg_mean_absolute_error"):
+            all_scores = dict()
+            for key, estimator in pipelines.items():
+                cv = GroupShuffleSplit(n_splits=2, train_size=.5, test_size=.5)
+                scores = cross_val_score(X=X, y=y, estimator=estimator,
+                                        cv=cv, n_jobs=min(2, n_jobs),
+                                        groups=groups,
+                                        scoring=scoring)
+                if scoring == 'neg_mean_absolute_error':
+                    scores = -scores
+                all_scores[key] = scores
+            score_name = scoring if scoring == 'r2' else 'mae'
+            np.save(op.join('data',
+                            f'all_scores_models_fieldtrip_spoc_{score_name}.npy'),
+                    all_scores)
+        
+        exp.append [i, func_args, raw, eda, ec, y, y_preds]
+        
+    return exp
+        
+#%%
+experiment_results = pipeline_all_subjects(number_subject=['01', '02'],
+                                           crop = True,
+                                           tune_components = False)
 
-run_experiments.pipeline_all_subjects(number_subject='01')
+#%%
+fig, ax = plt.subplots(1, 1, figsize=[10, 4])
+times = raw.times[ec.events[:, 0] - raw.first_samp]
+ax.plot(times, y_preds, color='b', label='Predicted EDA', linewidth=2, alpha=0.3)
+ax.plot(times, y, color='r', label='True EDA', linewidth=2, alpha=0.3)
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('EDA mean')
+ax.set_title('Riemann EDA Predictions')
+plt.legend()
+#plt.xlim(0,300)
+mne.viz.tight_layout()
+plt.show()
 
+# %%
+"""
+Integrar todo en la funcion final
+-  Dataframe: use itertools
+-  Que se pueda correr en Drago
 
+"""
